f87dd2ede1775e76f1c8e56bf1b4550592b2ff07
salaheddineS <SALAHeddinemorchid1@gmail.com>
2024-01-12 13:13:35 +0100

    [SERVICES] Added Mock service to prepare pipeline

---
+++ web-services/gestion-service/src/main/java/org/sid/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,25 @@
+package org.sid.gatewayservice;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+														DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
+
+
+}
--- web-services/gestion-service/src/test/java/org/sid/gatewayservice/GatewayServiceApplicationTests.java
+++ web-services/gestion-service/src/test/java/org/sid/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.gestiondesrh;
+package org.sid.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionDesRhApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 
 	}
 
 }
